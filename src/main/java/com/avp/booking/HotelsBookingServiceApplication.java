58db21a3e765f1fee51948f4d63bdd35c430be61
Andrei Vasilevich <vasilevich@azoft.com>
2017-05-10 20:24:44 +0600

    Clear

--- src/main/java/com/avp/booking/HotelsBookingServiceApplication.java
+++ src/main/java/com/avp/booking/HotelsBookingServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelsBookingServiceApplication {
-
+public class HotelsBookingServiceApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(HotelsBookingServiceApplication.class, args);
 	}
